20ee952623d384a5dcf9dcc68c8630f7a8d27359
Administrator <Administrator@134.154.17.115>
2018-10-17 14:27:17 -0700

    Skeleton of the assignment

---
+++ test/GeneralTests.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class GeneralTests {
+    
+    public GeneralTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test 
+    public void setup(){
+        // checking to see if everyone has necessary game components
+    }
+    
+    @Test 
+    public void validmove(){
+        // if it does one and only one of the three possible moves
+    }
+    
+    @Test
+    public void scorecheck(){
+        // checks to see who is winning
+    }
+    
+    @Test
+    public void route(){
+        // if the route is valid
+        // if the route is available
+    }
+    
+    @Test
+    public void endcheck(){
+        // checks to see if the game is over
+    }
+            
+}
