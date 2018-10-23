96f1ccfde9491f8f6ead76b1499fd2c5bbbf13cd
Administrator <Administrator@134.154.17.115>
2018-10-23 13:37:14 -0700

    Added a train deck function

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -5,6 +5,9 @@
  */
 package finalproject;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author Administrator
@@ -14,6 +17,14 @@ public class FinalProject {
     /**
      * @param args the command line arguments
      */
+    public static List train_deck(){                // A function that return random cards in a list
+        List tdeck = null;
+        Random rnd = new Random();
+        for(int i = 0; i < 5; i++)
+            tdeck.add(i, rnd.nextInt(200) + 1);
+        return tdeck;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here\
         System.out.println("TICKET TO RIDE");
