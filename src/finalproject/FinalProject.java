f60d4180ab18702f5b5156c3d060cbf79455cab0
Administrator <Administrator@134.154.17.115>
2018-10-24 10:35:59 -0700

    Added route deck and modified train deck plus added a face up function.

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -5,6 +5,7 @@
  */
 package finalproject;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -17,12 +18,48 @@ public class FinalProject {
     /**
      * @param args the command line arguments
      */
-    public static List train_deck(){                // A function that return random cards in a list
-        List tdeck = null;
-        Random rnd = new Random();
-        for(int i = 0; i < 5; i++)
-            tdeck.add(i, rnd.nextInt(200) + 1);
-        return tdeck;
+    
+    public static List train_deck(){        // A function that return random cards in a list
+        List deck = null;                   //deck to be returned
+        List tdeck = null;                  //orinal train deck
+        for(int i = 0; i < 110; i++)        // First add card numbers into the list
+            tdeck.add(i);
+        Collections.shuffle(deck);          // Shuffle it like a normal physical deck.
+        
+        for(int i = 0; i < 4; i++){
+            deck.add(i, tdeck.get(i));      // grab the first 4 cards to distribute to player.
+            tdeck.remove(i);                // remove cards from the orignal deck
+        }
+        
+        face_up(tdeck);                     //call for cards to be faced up.
+        
+        tdeck.clear();                      //clears the list for a reshuffle 
+        return deck;                        //return the deck list to the player
+    }
+    
+    public static List face_up(List up){    //Returns cards to be faced up.
+        List Faces = null;
+        for(int i = 0; i < 5; i++){
+            Faces.add(up.get(i));
+        }
+        
+        return Faces;
+    }
+    
+    public static List route_deck(){
+        List deck = null;                   //deck to be returned
+        List rdeck = null;                  //orignal route deck
+        for(int i = 0; i < 30; i++)        // First add card numbers into the list
+            rdeck.add(i);
+        Collections.shuffle(deck);          // Shuffle it like a normal physical deck.
+        
+        for(int i = 0; i < 3; i++){
+            deck.add(i, rdeck.get(i));      // grab the first 3 cards to distribute to player.
+            rdeck.remove(i);                // remove cards from the orignal deck
+        }
+        
+        rdeck.clear();                      //clears the list for a reshuffle 
+        return deck;                        //return the deck list to the player
     }
     
     public static void main(String[] args) {
