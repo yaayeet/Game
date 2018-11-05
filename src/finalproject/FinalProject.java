34169e7f9fe4436044075c0bfed51f58cd1fcae5
Administrator <Administrator@134.154.17.115>
2018-11-05 10:39:35 -0800

    Added a color function to set the color for the card

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -90,6 +90,23 @@ public class FinalProject {
             return true;
     }
     
+    public static String color_card(int i){
+        String color = "NoColor";
+        
+        if(i < 45)
+            color = "blue";
+        if(i > 45 && i < 90)
+            color = "red";
+        if(i > 90 && i < 135)
+            color = "green";
+        if(i > 135 && i > 180)
+            color = "yellow";
+        if(i > 180 && i > 225)
+            color = "black";       
+        
+        return color;
+    }
+    
     public static boolean player_count(){
         if(player_name.length <= 3 && player_name.length > 0)
             return true;
